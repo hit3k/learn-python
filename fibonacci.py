1048b473cb6b98eb12420881cd62fff7f533b372
tanya <tanya@molly.local>
2026-08-14 21:33:37 +1000

    numbers

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+# Most flowers have a Fibonacci number of petals.
+
+f0 = 0
+f1 = 1
+
+print ( "Fibonacci: " + str( f0  ) + ", " + str( f1 ))
+
+f2 = f0 + f1
+print ( "Next: " + str( f2 ))
+
+
---
+++ triangular.py
@@ -0,0 +1,10 @@
+#  Triangular number sequence - pack circles in a triangle.
+
+print ( "Next: " + str( 1 + 2 ))
+
+t1 = 1
+t2 = 2
+
+t3 = t1 + t2
+print ( "Next: " + str( t3 ))
+
